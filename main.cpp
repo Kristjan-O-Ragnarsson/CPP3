ebeb739d286ea10285c1ab079342a1e11d9dfc3f
Natsumi2813 <kristjan@ragnars.net>
2018-10-23 17:47:45 +0000

    added addPassengers and removePassengers

--- main.cpp
+++ main.cpp
@@ -8,27 +8,52 @@ public:
 	FlightBooking(int id, int capacity, int reserved);
 	//~FlightBooking();
 	void printStatus();
+	bool addPassengers(int n);
+	bool removePassengers(int n);
 private:
 	int getFillLevel();
-	int id;
-	int capacity;
-	int reserved;
+	int id_m;
+	int capacity_m;
+	int reserved_m;
 };
 void FlightBooking::printStatus()
 {
-	cout << "Flight " << id << " :" << reserved << "/" << capacity << "("<< getFillLevel() << "%)";
+	cout << "Flight " << id_m << " :" << reserved_m << "/" << capacity_m << "("<< getFillLevel() << "%)";
 }
 
 int FlightBooking::getFillLevel(){
-	return 100 * reserved / capacity;
+	return 100 * reserved_m / capacity_m;
 
 }
 
 FlightBooking::FlightBooking(int id, int capacity, int reserved)
 {
-	this->id = id;
-	this->capacity = capacity;
-	this->reserved = reserved;
+    if(reserved < abs(reserved)){
+        reserved = 0;
+    }
+	id_m = id;
+	capacity_m = capacity;
+	reserved_m = reserved;
+}
+
+bool FlightBooking::addPassengers(int n) {
+    if ( (reserved_m + n) < (reserved_m * 1.05)){
+        reserved_m += n;
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+bool FlightBooking::removePassengers(int n) {
+    if (reserved_m - n >= 0){
+        reserved_m -= n;
+        return true;
+    } else {
+        return false;
+    }
+
 }
 
 /*
@@ -37,6 +62,7 @@ FlightBooking::~FlightBooking(){
 }
 */
 int main() {
+    string input;
 	int reserved = 0,
 		capacity = 0;
 	cout << "Provide flight capacity: ";
@@ -47,8 +73,10 @@ int main() {
 	booking.printStatus();
 	while (true)
 	{
-		//cin
-		// some function for prasing
+		cin >> input;
+		if (1){
+            break;
+		}
 	}
 	return 0;
 }
