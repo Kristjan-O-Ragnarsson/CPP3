2699fe3adf525bb78d67c8f1e092febbb64b5a62
Natsumi2813 <kristjan@ragnars.net>
2018-10-23 23:42:14 +0000

    added flight pool

---
+++ FlightPool.cpp
@@ -0,0 +1,4 @@
+//
+// Created by new pc on 23/10/2018.
+//
+
---
+++ FlightPool.h
@@ -0,0 +1,19 @@
+//
+// Created by new pc on 23/10/2018.
+//
+
+#ifndef CPP3_FLIGHTPOOL_H
+#define CPP3_FLIGHTPOOL_H
+
+class FlightPool {
+private:
+    int *arr_m;
+    int size_m;
+public:
+    FlightPool();
+    ~FlightPool();
+    //getFlightPointer();
+    //add
+};
+
+#endif //CPP3_FLIGHTPOOL_H
